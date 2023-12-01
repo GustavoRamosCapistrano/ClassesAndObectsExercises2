c7a9af5b5c9d6e2d95b3b01da44ad9539251f2be
Gustavo Ramos Capistrano <sba23064@student.cct.ie>
2023-12-01 16:33:47 +0000

    Made car1 and contructor

--- src/classesandobectsexercises2/ClassesAndObectsExercises2.java
+++ src/classesandobectsexercises2/ClassesAndObectsExercises2.java
@@ -16,6 +16,9 @@ public class ClassesAndObectsExercises2 {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Hello Class");
+        
+        Car car1 = new Car("Honda", "Civic", 4 , "Yellow");
+        car1.drive(90);
     }
     
 }
